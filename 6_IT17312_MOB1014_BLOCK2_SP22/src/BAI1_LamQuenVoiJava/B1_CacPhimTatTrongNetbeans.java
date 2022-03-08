eeb6d309eaa7bd7ec05669cc324e40ebb4d0cc83
dungna29 <dungna29@localhost>
2022-03-08 14:00:54 +0700

    Dungna 2022

---
+++ 6_IT17312_MOB1014_BLOCK2_SP22/src/BAI1_LamQuenVoiJava/B1_CacPhimTatTrongNetbeans.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package BAI1_LamQuenVoiJava;
+
+/**
+ *
+ * @author dungna29
+ */
+public class B1_CacPhimTatTrongNetbeans {
+    //psvm + tab = Gọi main
+    public static void main(String[] args) {
+        //sout + tab
+        System.out.println("Chào các bạn đến với java 1");
+       
+       
+    }
+}
