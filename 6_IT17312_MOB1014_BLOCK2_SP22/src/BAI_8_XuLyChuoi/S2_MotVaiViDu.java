1c0ed91b5769212f17b4d3b8f9f720534aa7f353
dungna29 <dungna29@localhost>
2022-04-05 12:16:17 +0700

    Dungna 2022

---
+++ 6_IT17312_MOB1014_BLOCK2_SP22/src/BAI_8_XuLyChuoi/S2_MotVaiViDu.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package BAI_8_XuLyChuoi;
+
+/**
+ *
+ * @author dungna29
+ */
+public class S2_MotVaiViDu {
+    //Bài 1: Viết 1 chương trình nhập vào Full Name sao đó viết hoa chữ cái đầu của từ lên.  nguyen thi hoa = Nguyen Thi Hoa
+    //Bài 2: Viết 1 chương trình zen mã tự động như sinh viên tại FPOLY.  nguyễn thị hoa = HoaNTPHXXX
+    //Giải quyết 2 bài trên dựa vào lý thuyết xử lý chuỗi thầy đã up.
+}
